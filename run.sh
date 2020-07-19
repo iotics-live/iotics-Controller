d57d96b4bc52cd9242d600f0ecd6c3d546ab5d88
Hussain <51000019+Po0wnage@users.noreply.github.com>
2020-07-19 15:14:25 +0300

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,22 +1,3 @@
-#!/bin/bash
+#!/usr/bin/with-contenv bashio
 
-mkdir -p /share/iotics
-mkdir -p /share/iotics/mysql
-chmod -R 777 /share/iotics/mysql
-rm -rf /var/lib/mysql
-ln -s /share/iotics/mysql /var/lib/
-cd /var/lib/mysql
-mysql_install_db
-
-/etc/init.d/mysql start
-
-mysql -e "CREATE DATABASE IF NOT EXISTS ioticsdb ; \
-        CREATE USER IF NOT EXISTS 'iotics_dbuser'@'localhost' IDENTIFIED BY 'IoTics_Db_(!?11111)'; \
-        GRANT ALL PRIVILEGES  ON ioticsdb.* TO 'iotics_dbuser'@'localhost'; \
-        FLUSH PRIVILEGES; \
-        ALTER DATABASE ioticsdb CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
-
-/etc/init.d/nginx start
-
-cd /app
-dotnet Iotics.Api.dll
+echo Hello world!
